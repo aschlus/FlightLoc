dccce615a091b1a928bef505605f84e81ec7591f
Andrew Schlusberg <aschlus@optonline.net>
2020-08-12 17:26:16 -0400

    Created default activity

---
+++ app/src/main/java/ashex/flightloc/LocationList.kt
@@ -0,0 +1,11 @@
+package ashex.flightloc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_list)
+    }
+}
